20696fb677f555fcc3c25dc47603f0be2742da58
Rajwinder03334 <145788780+Rajwinder03334@users.noreply.github.com>
2024-01-29 14:08:32 +0200

    Initial commit

---
+++ area.py
@@ -0,0 +1,4 @@
+area_in_square_meters = float(input("Enter the area of the house in square meters: "))
+square_feet = area_in_square_meters * 10.764
+if area_in_square_meters >= 0:
+    print(square_feet)
---
+++ score.py
@@ -0,0 +1,3 @@
+student_score = float(input("Enter the student's score: "))
+if student_score >= 90:
+    print("The student's grade is: A")
